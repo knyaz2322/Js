f3eafc58186bf899a7d63cf7c2c6c3a731575819
Dostonbek <dostonbeksodikov8@gamil.com>
2024-05-26 19:19:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var name = prompt(" Ismingizni kiriting ")
+console.log(" Your name " + name);
+
+var age = +prompt( " Yoshingizni kiriting " )
+console.log(" Your age "  + age );
+
+var num = +prompt( "100 + 20=?" )
+if ( num >= 120 ) {
+    console.log(" Sizning javob togri 120 ");
+}else {
+    console.log( " Sizning javob natogri  Togri javob esa 120 ");
+}
+
+var num = +prompt( "20 - 10=?" )
+if ( num >=10) {
+    console.log( " Sizning javob togri 10 " );
+}else{
+    console.log( "Sizning javob natogri togri javob 10" );
+}
+
+var num = +prompt( "10 * 55=?" )
+if ( num >=550) {
+    console.log( " Sizning javob togri 550 " );
+}else{
+    console.log( " Sizning javob natogri togri javob 550 " );
+}
+
+var num = +prompt( "12/2=?" )
+if ( num >=6) {
+    console.log( " Sizning javob togri 6 " );
+}else{
+    console.log( " Sizning javob natogri togri javob 6 " );
+}
+
+var num = +prompt( "50 % 2=?" )
+if ( num >=1) {
+    console.log( " Sizning javob togri 1 " );
+}else{
+    console.log( " Sizning javob natogri togri javob 1 " );
+}
